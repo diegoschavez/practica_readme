54270bd3e0ae6bc6e532d70e47db7e71da656714
diegoschavez <143551057+diegoschavez@users.noreply.github.com>
2024-11-27 10:12:09 -0600

    Add files via upload

---
+++ imprimir.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<fstream>
+
+struct Person{
+  int id;
+  std::string first_name;
+  std::string last_name;
+  char genre;
+  int dad;
+  int mom;
+};
+main
+
+Person agregar_datos(){
+  std::cout<<"Ingese id: ";
+  std::cin>>id;
+
+};
